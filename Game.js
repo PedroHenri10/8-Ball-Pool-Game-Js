3bc976d54ef2c36bcb7c4e7a280fc90ca0339cae
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-08-31 20:12:41 -0300

    refatoracao mesa

--- Game.js
+++ Game.js
@@ -3,13 +3,22 @@ function Game(){
 }
 
 Game.prototype.init = function(){
-
+    this.gameWorld = new GameWorld();
 }
 
 Game.prototype.start = function(){
+    PoolGame.init();
+
+    PoolGame.mainLoop();
 
 }
 
 Game.prototype.mainLoop = function(){
-    
+    Canvas.clear();
+    PoolGame.gameWorld.update();
+    PoolGame.gameworld.draw();
+
+    requestAnimationFrame(PoolGame.mainLoop);
 }
+
+let PoolGame = new Game();
