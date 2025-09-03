8c067f299aa48be819398de03be1f82d78998563
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-02 21:10:45 -0300

    vector2

--- GameWorld.js
+++ GameWorld.js
@@ -1,23 +1,39 @@
 const DELTA = 1/100;
 
 function GameWorld(){
-    this.whiteBall = new Ball(new Vector2(413, 413), COLOR.WHITE);
+    this.whiteBall = this.balls[this.balls.length -1];
     this.stick = new Stick(new Vector2(413, 413), this.whiteBall.shoot.bind(this.whiteBall));
 }
 
 GameWorld.prototype.update = function(){
     this.stick.update();
-    this.whiteBall.update(DELTA);
+    for(let i = 0; i < this.balls.length ; i++){
+        this.Balls[i].update(DELTA);
+    }
+
+    if(!this.ballsMoving() && this.stick.shot){
+        this.stick.reposition(this.whiteBall.position);
+    }
 }
 
 GameWorld.prototype.draw = function(){
     Canvas.drawImage(sprites.background, {x:0, y:0});
 
+    for(let i = 0; i < this.balls.length ; i++){
+        this.Balls[i].draw(DELTA);
+    }
+
     this.stick.draw();
-    this.whiteBall.draw();
+    
 }
 
-GameWorld.prototype.update = function(){
-    this.stick.update();
-    this.whiteBall.moving;
+GameWorld.prototype.ballsMoving = function(){
+    let ballsMoving = false;
+
+    for(let i = 0; i< this.balls.length ; i++){
+        if(this.Balls[i].moving){
+            ballsMoving = true;
+            break;
+        }
+    }
 }
