3538de8a74792f8caf515865379e26c2cbee627d
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-08 18:39:48 -0300

    refatoracao assests

--- Assets.js
+++ Assets.js
@@ -1,42 +1,45 @@
-
 var sprites = {};
 let assetsStillLoading = 0;
 
 function assetsLoadingLoop(callback){
-
-    if(assetsStillLoading){
+    if(assetsStillLoading > 0){
         requestAnimationFrame(assetsLoadingLoop.bind(this, callback));
-    }else{
+    } else {
         callback();
     }
 }
 
 function loadAssets(callback){
-
-    function loadSprite(fileName){
+    function loadSprite(fileName, name){
         assetsStillLoading++;
 
         let spriteImage = new Image();
         spriteImage.src = "./assets/sprites/" + fileName;
 
         spriteImage.onload = function(){
+            sprites[name] = spriteImage;
+            assetsStillLoading--;
+        }
+
+        spriteImage.onerror = function(){
+            console.error("Falha ao carregar sprite: " + spriteImage.src);
             assetsStillLoading--;
         }
 
         return spriteImage;
     }
-    sprites.background = loadSprite('spr_background5.png');
-    sprites.stick = loadSprite('spr_stick.png');
-    sprites.whiteBall = loadSprite('spr_whiteBall.png');
-    sprites.redBall = loadSprite('spr_redBall2.png');
-    sprites.yellowBall = loadSprite('spr_yellowBall2.png');
-    sprites.blackBall = loadSprite('spr_blackBall2.png');
+
+    loadSprite('spr_background5.png', 'background');
+    loadSprite('spr_stick.png', 'stick');
+    loadSprite('spr_whiteBall.png', 'whiteBall');
+    loadSprite('spr_redBall2.png', 'redBall');
+    loadSprite('spr_yellowBall2.png', 'yellowBall');
+    loadSprite('spr_blackBall2.png', 'blackBall');
 
     assetsLoadingLoop(callback);
 }
 
 function getBallSpriteByColor(color){
-
     switch(color){
         case COLOR.RED:
             return sprites.redBall;
@@ -46,5 +49,8 @@ function getBallSpriteByColor(color){
             return sprites.blackBall;
         case COLOR.WHITE:
             return sprites.whiteBall;
+        default:
+            console.warn("Cor de bola desconhecida:", color);
+            return null;
     }
 }
