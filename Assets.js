9d8ed0b1285bb7cbc4100bad9a8d87b8287f7089
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-13 02:36:48 -0300

    assets

--- Assets.js
+++ Assets.js
@@ -1,34 +1,30 @@
 var sprites = {};
 let assetsStillLoading = 0;
 
-function assetsLoadingLoop(callback){
-    if(assetsStillLoading > 0){
+function assetsLoadingLoop(callback) {
+    if (assetsStillLoading > 0) {
         requestAnimationFrame(assetsLoadingLoop.bind(this, callback));
     } else {
         callback();
     }
 }
 
-function loadAssets(callback){
-    function loadSprite(fileName, name){
-        assetsStillLoading++;
+function loadSprite(fileName, name) {
+    assetsStillLoading++;
+    const spriteImage = new Image();
+    spriteImage.src = "./assets/sprites/" + fileName;
+    spriteImage.onload = function () {
+        sprites[name] = spriteImage;
+        assetsStillLoading--;
+    };
+    spriteImage.onerror = function () {
+        console.error("Falha ao carregar sprite: " + spriteImage.src);
+        assetsStillLoading--;
+    };
+    return spriteImage;
+}
 
-        let spriteImage = new Image();
-        spriteImage.src = "./assets/sprites/" + fileName;
-
-        spriteImage.onload = function(){
-            sprites[name] = spriteImage;
-            assetsStillLoading--;
-        }
-
-        spriteImage.onerror = function(){
-            console.error("Falha ao carregar sprite: " + spriteImage.src);
-            assetsStillLoading--;
-        }
-
-        return spriteImage;
-    }
-
+function loadAssets(callback) {
     loadSprite('spr_background5.png', 'background');
     loadSprite('spr_stick.png', 'stick');
     loadSprite('spr_whiteBall.png', 'whiteBall');
@@ -39,18 +35,12 @@ function loadAssets(callback){
     assetsLoadingLoop(callback);
 }
 
-function getBallSpriteByColor(color){
-    switch(color){
-        case COLOR.RED:
-            return sprites.redBall;
-        case COLOR.YELLOW:
-            return sprites.yellowBall;
-        case COLOR.BLACK:
-            return sprites.blackBall;
-        case COLOR.WHITE:
-            return sprites.whiteBall;
-        default:
-            console.warn("Cor de bola desconhecida:", color);
-            return null;
+function getBallSpriteByColor(color) {
+    switch (color) {
+        case COLOR.RED: return sprites.redBall;
+        case COLOR.YELLOW: return sprites.yellowBall;
+        case COLOR.BLACK: return sprites.blackBall;
+        case COLOR.WHITE: return sprites.whiteBall;
+        default: return null;
     }
 }
