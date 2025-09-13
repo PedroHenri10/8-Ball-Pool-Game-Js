7f860b54be8c6e1ed48b2be6a758e293cc68a2c3
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-13 02:31:29 -0300

    posicao e calculos

--- Vector2.js
+++ Vector2.js
@@ -1,3 +1,4 @@
+// Vector2.js
 function Vector2(x = 0, y = 0) {
     this.x = x;
     this.y = y;
@@ -5,29 +6,35 @@ function Vector2(x = 0, y = 0) {
 
 Vector2.prototype.copy = function () {
     return new Vector2(this.x, this.y);
-}
+};
 
-Vector2.prototype.add = function (vector) {
-    return new Vector2(this.x + vector.x, this.y + vector.y);
-}
+Vector2.prototype.add = function (v) {
+    return new Vector2(this.x + v.x, this.y + v.y);
+};
 
-Vector2.prototype.addTo = function (vector) {
-    this.x += vector.x;
-    this.y += vector.y;
-}
+Vector2.prototype.addTo = function (v) {
+    this.x += v.x;
+    this.y += v.y;
+};
 
-Vector2.prototype.subtract = function (vector) {
-    return new Vector2(this.x - vector.x, this.y - vector.y);
-}
+Vector2.prototype.subtract = function (v) {
+    return new Vector2(this.x - v.x, this.y - v.y);
+};
 
-Vector2.prototype.mult = function (scalar) {
-    return new Vector2(this.x * scalar, this.y * scalar);
-}
+Vector2.prototype.mult = function (s) {
+    return new Vector2(this.x * s, this.y * s);
+};
 
-Vector2.prototype.dot = function (vector) {
-    return this.x * vector.x + this.y * vector.y;
-}
+Vector2.prototype.dot = function (v) {
+    return this.x * v.x + this.y * v.y;
+};
 
 Vector2.prototype.length = function () {
     return Math.sqrt(this.x * this.x + this.y * this.y);
-}
+};
+
+Vector2.prototype.normalize = function () {
+    const len = this.length();
+    if (len === 0) return new Vector2(0, 0);
+    return new Vector2(this.x / len, this.y / len);
+};
