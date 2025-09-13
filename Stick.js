3962bc106f3d814f552e75b8d2ec34ceffd31726
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-13 02:39:22 -0300

    logica taco

--- Stick.js
+++ Stick.js
@@ -3,7 +3,7 @@ const STICK_SHOT_ORIGIN = new Vector2(950, 11);
 const MAX_POWER = 7500;
 
 function Stick(position, onShoot) {
-    this.position = position;
+    this.position = position.copy ? position.copy() : new Vector2(position.x, position.y);
     this.rotation = 0;
     this.origin = STICK_ORIGIN.copy();
     this.power = 0;
@@ -12,43 +12,54 @@ function Stick(position, onShoot) {
 }
 
 Stick.prototype.update = function () {
-    if (Mouse.left.down) {
+    if (typeof Mouse !== 'undefined' && Mouse.left && Mouse.left.down) {
         this.increasePower();
     } else if (this.power > 0) {
         this.shoot();
     }
-
     this.updateRotation();
-}
+};
 
 Stick.prototype.draw = function () {
-    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
-}
+    if (sprites.stick) {
+        Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
+    } else {
+        
+        const ctx = Canvas.getContext();
+        if (!ctx) return;
+        ctx.save();
+        ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(this.rotation);
+        ctx.fillStyle = '#6b3';
+        ctx.fillRect(-5, -5, 60, 10);
+        ctx.restore();
+    }
+};
 
 Stick.prototype.updateRotation = function () {
-    let opposite = Mouse.position.y - this.position.y;
-    let adjacent = Mouse.position.x - this.position.x;
-
+    if (typeof Mouse === 'undefined' || !Mouse.position) return;
+    const opposite = Mouse.position.y - this.position.y;
+    const adjacent = Mouse.position.x - this.position.x;
     this.rotation = Math.atan2(opposite, adjacent);
-}
+};
 
 Stick.prototype.increasePower = function () {
-    if (this.power > MAX_POWER) {
-        return;
-    }
+    if (this.power > MAX_POWER) return;
     this.power += 120;
     this.origin.x += 5;
-}
+};
 
 Stick.prototype.shoot = function () {
-    this.onShoot(this.power, this.rotation);
+    if (typeof this.onShoot === 'function') {
+        this.onShoot(this.power, this.rotation);
+    }
     this.power = 0;
     this.origin = STICK_SHOT_ORIGIN.copy();
     this.shot = true;
-}
+};
 
 Stick.prototype.reposition = function (position) {
-    this.position = position.copy();
+    this.position = position.copy ? position.copy() : new Vector2(position.x, position.y);
     this.origin = STICK_ORIGIN.copy();
     this.shot = false;
-}
+};
