24787635f5b8ed0be3b8c57bd5a1d0cc85febd34
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-09-13 02:38:25 -0300

    fisica basica e colisao

--- Ball.js
+++ Ball.js
@@ -3,7 +3,7 @@ const BALL_DIAMETER = 30;
 const BALL_RADIUS = BALL_DIAMETER / 2;
 
 function Ball(position, color) {
-    this.position = position;
+    this.position = position.copy ? position.copy() : new Vector2(position.x, position.y);
     this.velocity = new Vector2();
     this.moving = false;
     this.color = color;
@@ -13,27 +13,25 @@ function Ball(position, color) {
 Ball.prototype.update = function (delta) {
     this.position.addTo(this.velocity.mult(delta));
     this.velocity = this.velocity.mult(0.984);
-
     if (this.velocity.length() < 5) {
         this.velocity = new Vector2();
         this.moving = false;
+    } else {
+        this.moving = true;
     }
-}
+};
 
 Ball.prototype.collideWithBall = function (ball) {
     const n = this.position.subtract(ball.position);
     const dist = n.length();
-
-    if (dist > BALL_DIAMETER) {
-        return;
-    }
+    if (dist === 0 || dist > BALL_DIAMETER) return;
 
     const mtd = n.mult((BALL_DIAMETER - dist) / dist);
 
     this.position = this.position.add(mtd.mult(0.5));
     ball.position = ball.position.subtract(mtd.mult(0.5));
 
-    const un = n.mult(1 / n.length());
+    const un = n.mult(1 / dist);
     const ut = new Vector2(-un.y, un.x);
 
     const v1n = un.dot(this.velocity);
@@ -41,59 +39,56 @@ Ball.prototype.collideWithBall = function (ball) {
     const v2n = un.dot(ball.velocity);
     const v2t = ut.dot(ball.velocity);
 
-    let v1nTag = v2n;
-    let v2nTag = v1n;
-
-    this.velocity = un.mult(v1nTag).add(ut.mult(v1t));
-    ball.velocity = un.mult(v2nTag).add(ut.mult(v2t));
+    this.velocity = un.mult(v2n).add(ut.mult(v1t));
+    ball.velocity = un.mult(v1n).add(ut.mult(v2t));
 
     this.moving = true;
     ball.moving = true;
-}
+};
 
 Ball.prototype.collideWithTable = function (table) {
     if (!this.moving) return;
 
     let collided = false;
-
     if (this.position.y <= table.TopY + BALL_RADIUS) {
         this.velocity = new Vector2(this.velocity.x, -this.velocity.y);
         collided = true;
     }
-
     if (this.position.x >= table.RightX - BALL_RADIUS) {
         this.velocity = new Vector2(-this.velocity.x, this.velocity.y);
         collided = true;
     }
-
     if (this.position.y >= table.BottomY - BALL_RADIUS) {
         this.velocity = new Vector2(this.velocity.x, -this.velocity.y);
         collided = true;
     }
-
     if (this.position.x <= table.LeftX + BALL_RADIUS) {
         this.velocity = new Vector2(-this.velocity.x, this.velocity.y);
         collided = true;
     }
-
-    if (collided) {
-        this.velocity = this.velocity.mult(0.98);
-    }
-}
+    if (collided) this.velocity = this.velocity.mult(0.98);
+};
 
 Ball.prototype.draw = function () {
-    Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
-}
+    if (this.sprite) {
+        Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
+    } else {
+        const ctx = Canvas.getContext();
+        if (!ctx) return;
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, BALL_RADIUS, 0, Math.PI * 2);
+        ctx.fillStyle = this.color || 'white';
+        ctx.fill();
+        ctx.closePath();
+    }
+};
 
 Ball.prototype.shoot = function (power, rotation) {
     this.velocity = new Vector2(power * Math.cos(rotation), power * Math.sin(rotation));
     this.moving = true;
-}
+};
 
-Ball.prototype.collideWith = function (object) {
-    if (object instanceof Ball) {
-        this.collideWithBall(object);
-    } else {
-        this.collideWithTable(object);
-    }
-}
+Ball.prototype.collideWith = function (obj) {
+    if (obj instanceof Ball) this.collideWithBall(obj);
+    else this.collideWithTable(obj);
+};
